cfbe8761ff004f085f859631eb9e2fab4f0d84fe
Jae Task <jae.task@task-cloud.com>
2013-07-26 23:38:50 +0100

    removing generated tests

--- test/multisync_test.js
+++ test/multisync_test.js
@@ -26,28 +26,5 @@ exports.multisync = {
   setUp: function(done) {
     // setup here if necessary
     done();
-  },
-	check_drives: function(test) {
-		test.expect(1);
-		test.equal();
-		test.done();
-	},
-  default_options: function(test) {
-    test.expect(1);
-
-    var actual = grunt.file.read('tmp/default_options');
-    var expected = grunt.file.read('test/expected/default_options');
-    test.equal(actual, expected, 'should describe what the default behavior is.');
-
-    test.done();
-  },
-  custom_options: function(test) {
-    test.expect(1);
-
-    var actual = grunt.file.read('tmp/custom_options');
-    var expected = grunt.file.read('test/expected/custom_options');
-    test.equal(actual, expected, 'should describe what the custom option(s) behavior is.');
-
-    test.done();
-  },
+  }
 };
