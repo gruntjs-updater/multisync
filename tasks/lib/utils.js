1fe6cc64c1086d351456069f69f9579c2fcd97c5
Jae Task <jae.task@task-cloud.com>
2013-07-26 23:35:15 +0100

    Added drive mounted checks

--- tasks/lib/utils.js
+++ tasks/lib/utils.js
@@ -9,56 +9,7 @@
 'use strict';
 
 module.exports = {
-
-	checkDriveConfig: function(grunt, data) {
-		// check inputs
-		if (typeof data.drives === 'undefined') {
-			grunt.log.error("drive config not found.");
-			grunt.fail.warn("You must configure some drive paths.");
-		}
-
-		if (typeof data.drives.src === 'undefined') {
-			grunt.log.error("src drive config not found.");
-			grunt.fail.warn("You must configure the source drive path.");
-		}
-
-		if (typeof data.drives.dest === 'undefined') {
-			grunt.log.error("dest drive config not found.");
-			grunt.fail.warn("You must configure the destination drive path.");
-		}
-	},
-
-	checkFoldersConfig: function(grunt, data) {
-		// check inputs
-		if (typeof data.folders === 'undefined') {
-			grunt.log.error("folder config not found.");
-			grunt.fail.warn("You must configure some folders to copy.");
-		}
-
-		for (var i=0, j=1; i < data.folders.length; i++, j++) {
-			if (typeof data.folders[i].src === 'undefined') {
-				grunt.log.error("src drive config not found for folder: "+j);
-				grunt.fail.warn("Please check to see if any folders are missing src parameters");
-			}
-
-			if (typeof data.folders[i].dest === 'undefined') {
-				grunt.log.error("dest drive config not found for folder: "+j);
-				grunt.fail.warn("Please check to see if any folders are missing dest parameters");
-			}
-		}
-	},
-
-	expandDrivePaths: function(grunt, data) {
-
-	},
-
-	checkDrivesMounted: function(grunt, data) {
-		this.driveMounted(grunt, 'src', data.drives.src);
-		this.driveMounted(grunt, 'dest', data.drives.dest);
-	},
-
-	// Utility functions
-
+	
 	driveMounted: function(grunt, name, mountPoint) {
 		if (this.folderExists(mountPoint) === false) {
 			grunt.log.error(name+' not mounted: '+mountPoint);
