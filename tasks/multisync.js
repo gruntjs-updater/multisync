c78b4a873688acf8b834a2b7be1476df7aba95b3
Jae Task <jae.task@task-cloud.com>
2013-07-26 23:15:11 +0100

    Valodating input config with utils library

--- tasks/multisync.js
+++ tasks/multisync.js
@@ -6,29 +6,17 @@
  * Licensed under the MIT license.
  */
 
+var utils = require('./lib/utils.js');
+
 'use strict';
 
 module.exports = function (grunt) {
 
 	grunt.registerMultiTask('multisync', 'Sync multiple folder pairs across locations', function () {
 
-		if (typeof this.data.drives === 'undefined') {
-			grunt.log.error("drive config not found.");
-			grunt.fail.warn("You must configure some drive paths.");
-		}
+		utils.checkDriveConfig(grunt, this.data);
+		utils.checkFoldersConfig(grunt, this.data);
 
-		if (typeof this.data.drives.src === 'undefined') {
-			grunt.log.error("src drive config not found.");
-			grunt.fail.warn("You must configure the source drive path.");
-		}
-
-		if (typeof this.data.drives.dest === 'undefined') {
-			grunt.log.error("dest drive config not found.");
-			grunt.fail.warn("You must configure the destination drive path.");
-		}
-
-
-		// load, check & process config..
 
 		grunt.log.writeln(JSON.stringify(this));
 		//grunt.log.writeln(JSON.stringify(grunt.config.get('multisync.job1')));return;
