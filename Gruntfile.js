2d51372c8dd34503a0f996df6404a721394cf06a
Jae Task <jae.task@task-cloud.com>
2013-07-28 13:28:39 +0100

    tweaking task runner

--- Gruntfile.js
+++ Gruntfile.js
@@ -79,6 +79,6 @@ module.exports = function(grunt) {
 
   grunt.registerTask('test', ['clean', 'shell', 'multisync:macbook', 'nodeunit']);
   grunt.registerTask('default', ['jshint', 'test']);
-	grunt.registerTask('multisync-rysnc', ['grunt-rsync']);
+	grunt.registerTask('multisync-rysnc', ['rsync']);
 
 };
